14cdde0be9c896c2b82ee69ea1cff437c30cb1a0
vishal <vishal@vishal-MacBook-Pro.local>
2018-04-26 13:59:57 +0530

    changes

--- integration/EmailTests.cs
+++ integration/EmailTests.cs
@@ -12,7 +12,7 @@ namespace integration
     public class EmailTests
     {
         public const string GeneratorApiRoot = "http://generator";
-        public const string MailHogApiV2Root = "http://mail:8025/api/v2";
+        public const string MailHogApiV2Root = "http://localhost:8025/api/v2";
 
         [Fact]
         public async Task SendEmailWithNames_IsFromGenerator()
